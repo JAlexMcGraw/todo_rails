9546451ca210257f18bd81737609a0f311da4759
Alex McGraw <jamcgraw21@gmail.com>
2025-04-05 21:52:28 -0400

    changed about page

--- test/controllers/todo_list_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TodoListControllerTest < ActionDispatch::IntegrationTest
-  test "should get todoTable" do
-    get todo_list_todoTable_url
-    assert_response :success
-  end
-end
